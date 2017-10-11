7891cd4741dcce39b9fb318c3f3f579b9749504a
Rui Lopes <rgl@ruilopes.com>
2017-10-11 08:09:10 +0100

    wait for the portainer container to be running before using it

--- provision-portainer.sh
+++ provision-portainer.sh
@@ -11,7 +11,7 @@ docker service create \
     portainer/portainer \
         -H unix:///var/run/docker.sock
 
-# dump the version.
+# get the first expected container name.
 container_name="portainer.1.$(
     docker service ps \
         --no-trunc \
@@ -20,4 +20,9 @@ container_name="portainer.1.$(
         -f name=portainer.1 \
         portainer \
         | head -1)"
+
+# wait for the container to be running.
+bash -c "while [ -z \"\$(docker ps -q -f 'name=$container_name' -f status=running)\" ]; do sleep 1; done"
+
+# dump the portainer version.
 docker exec $container_name /portainer --version
