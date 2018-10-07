1e7775c1457e012c2e55b69dbca5fd031cf2bdce
Rui Lopes <rgl@ruilopes.com>
2018-10-07 13:45:18 +0100

    explicitly disable docker debug and experimental mode

--- provision-docker.sh
+++ provision-docker.sh
@@ -18,6 +18,8 @@ apt-get install -y docker-ce=18.06.1~ce~3-0~ubuntu
 systemctl stop docker
 cat >/etc/docker/daemon.json <<'EOF'
 {
+    "experimental": false,
+    "debug": false,
     "labels": [
         "os=linux"
     ],
